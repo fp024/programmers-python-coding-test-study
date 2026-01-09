468bd195f39568543be39e6337a52689dc3c0632
fp024 <aopenasus@naver.com>
2026-01-09 23:14:20 +0900

    pre-commit용 pytest 실패 테스트 설명 보완

--- tests/test_temp_fail.py
+++ tests/test_temp_fail.py
@@ -1,2 +1,5 @@
-def test_fail():
-    assert True  # NOSONAR : 성공으로 변경
+def test_fail_for_pre_commit_pytest():
+    """
+    pre-commit의 push 직전 pytest 실패 테스트 용
+    """
+    assert True  # NOSONAR : 확인 필요시 False로 변경
